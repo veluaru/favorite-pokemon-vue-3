17089e3a23efd2f02dbfcd78db9bde7173458abc
Angie Castañeda <angie.castaneda@guane.com.co>
2025-11-21 12:09:10 -0500

    :lipstick: Fix in heade image

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' 
+    ? '/favorite-pokemon-vue-3/' 
+    : '/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
